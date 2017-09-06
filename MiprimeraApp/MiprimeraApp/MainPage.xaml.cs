602e93638bd36bc6c8a386fec96bbc8c85c23c3f
Eduardo Campos S <Eduardo Campos S>
2017-09-05 23:20:21 -0500

    Add project files.

---
+++ MiprimeraApp/MiprimeraApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MiprimeraApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void btn_uno(object sender, EventArgs args)
+        {
+            await Navigation.PushModalAsync(new Navegacion());
+        }
+    }
+}
---
+++ MiprimeraApp/MiprimeraApp/Navegacion.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MiprimeraApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Navegacion : NavigationPage
+	{
+		public Navegacion ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
